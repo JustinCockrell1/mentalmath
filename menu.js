4a71a6c05de210ea7903b23626304700a6c885d3
JustinCockrel1 <105675389+JustinCockrel1@users.noreply.github.com>
2024-07-21 04:45:24 -0500

    change to a

--- menu.js
+++ menu.js
@@ -107,15 +107,15 @@ const possibleOptions = [
 function init() {
     for(let i = 0; i < possibleOptions.length; i++) {
         console.log(possibleOptions[i].name, i);
-        optionsDiv.innerHTML += `<button class="option-choice">${possibleOptions[i].name}</button>`
+        optionsDiv.innerHTML += `<a href=/mentalmath.html?type=${i}><button class="option-choice">${possibleOptions[i].name}</button></a>`
     }
-    const optionDivs = document.querySelectorAll(".option-choice");
-    for(let i = 0; i < optionDivs.length; i++) {
-        optionDivs[i].onclick = function() {
-            window.location.href="/mentalmath/mentalmath.html?type=" + i;
-            // start();
-        }
-    }
+    // const optionDivs = document.querySelectorAll(".option-choice");
+    // for(let i = 0; i < optionDivs.length; i++) {
+    //     optionDivs[i].onclick = function() {
+    //         window.location.href="/mentalmath/mentalmath.html?type=" + i;
+    //         // start();
+    //     }
+    // }
     console.log("choose an option");
 }
 
